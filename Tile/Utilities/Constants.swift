9a444e7f460369ce7554aa904161e1ad067ac9aa
LinJian <linjian@ling.ai>
2017-12-28 11:20:59 +0800

    add Constants

---
+++ Tile/Utilities/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  Tile
+//
+//  Created by 小白 on 2017/12/28.
+//  Copyright © 2017年 LinJian. All rights reserved.
+//
+
+import SpriteKit
+
+let kDebug = true
+
+let kViewSize = UIScreen.main.bounds.size
+
+let kScreenCenter = CGPoint(x: kViewSize.width / 2, y: kViewSize.height / 2)
